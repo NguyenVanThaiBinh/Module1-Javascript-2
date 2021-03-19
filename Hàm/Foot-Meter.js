2a9393136e1e2cfc47b3f9d808d63ca9f7167ac5
Binh <nguyenvanthaibinh2210@gmail.com>
2021-03-19 08:59:29 +0700

    meter foot conver

---
+++ Hàm/Foot-Meter.js
@@ -0,0 +1,6 @@
+function ConvertToMeter(foot) {
+  return 0.305 * foot;
+}
+function ConvertToFoot(meter) {
+  return 3.279 * meter;
+}
--- Hàm/isPrime.js
+++ Hàm/isPrime.js
@@ -1,6 +1,4 @@
 function isPrime(number) {
-//   if (!Number.isInteger(number) || number <= 0) return false;
-
   for (let i = 2; i < number; i++) {
     if (number % i == 0) return false;
     return true;
